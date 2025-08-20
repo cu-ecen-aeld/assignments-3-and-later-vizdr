e0907313ccbde4d715841c775ad9a79fed3a6db3
Vladimir Zdravkov <vladzdravkovu@gmail.com>
2025-08-20 18:03:09 +0200

    test username

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,7 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* hardCodedUN = my_username();
+    const char* configUN = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardCodedUN, configUN, "Hard coded user name and configured one are not equal.");
 }
